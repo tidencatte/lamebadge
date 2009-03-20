3f738a4ef2d70d1302f7d9e5fb2065d528c26fa5
hydrapheetz <hydrapheetz@gmail.com>
2009-03-20 17:20:21 -0500

    Whoops

--- lamebadge.php
+++ lamebadge.php
@@ -1,6 +1,10 @@
 <?php
-	$repo     = $_GET["repo"];
-	$username = $_GET["username"];
+	$repo     = $_GET["repo"] or null;
+	$username = $_GET["username"] or null;
+
+	if (($repo === null) or ($username === null)) {
+		die("UH OH!");
+	}
 
 	$url   = curl_init("http://github.com/api/v1/json/$username/$repo/commit/master");
 	curl_setopt($url, CURLOPT_HEADER, false);
@@ -29,7 +33,7 @@
 		imagestring($image,$font,$x,$y, $string, $color);
 	}
 
-	$image = imagecreatetruecolor(240, 90);
+	$image = imagecreatetruecolor(240, 40);
 	$FILL_ALPHA       = imagecolorallocatealpha($image, 0, 0, 0, 50);
 	$FILL             = imagecolorallocatealpha($image, 255,255,255, 127);
 	$FILL_TEXT        = imagecolorallocatealpha($image, 255,255,255,0);
@@ -37,7 +41,7 @@
 
 	header("content-type: image/png");
 	dither_fill($image, $FILL);
-	shadow_text($image, 1, 1, 5, "Git commits!", $FILL_TEXT, $FILL_TEXT_SHADOW);
+	shadow_text($image, 1, 1, 5, $repo, $FILL_TEXT, $FILL_TEXT_SHADOW);
 
 	// show only the first six characters of the commit id
 	shadow_text($image, 8, 13, 3, "Master [".substr($arr['commit']['id'], 0, 6)."]", $FILL_TEXT, $FILL_TEXT_SHADOW);
